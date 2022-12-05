da658032b15fde65209f9ad307ee6cda389eafa1
Dmitriy Grey <ssseryyy.ru@gmail.com>
2022-12-05 14:30:52 +0300

    modified:   PR-7 1.12.2022/index.html 	modified:   PR-7 1.12.2022/js/script1.js

--- PR-7 1.12.2022/js/script1.js
+++ PR-7 1.12.2022/js/script1.js
@@ -1,37 +1,40 @@
+// Функция отказ
 function End(){
     alert('Хорошего вам дня, до свидания');
 }
 
 
 var year;
-year = 12%12
-function cYear(){
+year = 12 % 12;
 
-     if (year=1) {
-document.getElementById('god-content').innerHTML = "Вы родились в год Обезьяны вы очень энергичны и стремительны " + '<br>' + "<img src='img/monkey.jpg' width='450' height='450'>";
-        }else if(year=1) {
+// Функция рассчета года зодиака
+function cYear() {
+    
+        if ( year = 1 ) {
+            document.getElementById('god-content').innerHTML = "Вы родились в год Обезьяны вы очень энергичны и стремительны " + '<br>' + "<img src='img/monkey.jpg' width='450' height='450'>";
+        } else if ( year = 1 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=3) {
-        document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=4) {
+        } else if ( year = 3 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=5) {
+        } else if ( year = 4 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=6) {
+        } else if ( year = 5 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=7) {
+        } else if ( year = 6 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=8) {
+        } else if ( year = 7 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=9) {
+        } else if ( year = 8 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=10) {
+        } else if ( year = 9 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=11) {
+        } else if ( year = 10 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else if(year=12) {
+        } else if ( year = 11 ) {
             document.getElementById('Year').innerHTML = "Вы родились в год " ;
-        }else{
+        } else if ( year = 12 ) {
+            document.getElementById('Year').innerHTML = "Вы родились в год " ;
+        } else {
             document.getElementById('Year').innerHTML = "Error";
         }
 }
@@ -40,8 +43,12 @@ function Open() {
 }
 function openYear() {
     document.getElementById('trtBlokId').innerHTML = '<div class="godina">' + '<p>Введите год рождения</p>' + '<input type="date" name="year" ><button class="button button12" onclick="cYear()" >Подтвердить</button>' + '<p id="god-content"></p>' + '<img id="avatarka"></div>';
-    //document.getElementById('trtBlokId').innerHTML = ''
+    document.getElementById('refreshId').innerHTML = '<p>Хотите новую дату?</p>' +'<button class="button button2" onclick="Clear()"></button>Да' + '<button class="button button1" onclick="End()"></button>Нет';
 }
 function openZod() {
     document.getElementById('trtBlokId').innerHTML = '<div class="zodiak">' + '<p>Введите дату рождения</p>' + '<input type="date" name="" id="month"><button class="button button21" onclick="cZod()" >Подтвердить</button>' + '<p id="zodiak-content"></p></div>';
+    document.getElementById('refreshId').innerHTML = '<p>Хотите новую дату?</p>' +'<button class="button button2" onclick="Clear()"></button>Да' + '<button class="button button1" onclick="End()"></button>Нет';
+}
+function Clear() {
+    document.getElementById('clearId').innerHTML = '<div class="prvblok"><h1>Хотите знать о себе?</h1>' + '<button class="button button1" onclick="Open()" ></button>Да   ' + '<button class="button button2" onclick="End()" ></button>Нет</div>' + '<div class="vtrblok" id="vtrBlokId"></div>' + '<div class="trtblok" id="trtBlokId"></div>' + '<div class="refresh" id="refreshId"></div>';
 }
