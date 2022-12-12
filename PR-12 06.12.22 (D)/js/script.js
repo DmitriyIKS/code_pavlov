b657155639c51cd1e143bbb4cfb65cb9bcec35d1
Dmitriy Grey <ssseryyy.ru@gmail.com>
2022-12-12 23:23:41 +0300

    new file:   PR-12 06.12.22 (D)

---
+++ PR-12 06.12.22 (D)/js/script.js
@@ -0,0 +1,124 @@
+class Slider {
+    constructor(slider, autoplay = true) {
+        // элемент div.carousel
+        this.slider = slider;
+        // все кадры (слайды)
+        this.allFrames = slider.querySelectorAll('.carousel-item');
+        // цепочка кадров
+        this.frameChain = slider.querySelector('.carousel-slides');
+        // кнопка «вперед»
+        this.nextButton = slider.querySelector('.carousel-next');
+        // кнопка «назад»
+        this.prevButton = slider.querySelector('.carousel-prev');
+
+        this.index = 0; // индекс кадра, который сейчас в окне просмотра
+        this.length = this.allFrames.length; // сколько всего есть кадров
+        this.autoplay = autoplay; // включить автоматическую прокрутку?
+        this.paused = null; // чтобы можно было выключать автопрокрутку
+
+        this.init(); // инициализация слайдера
+    }
+
+    init() {
+        this.dotButtons = this.dots(); // создать индикатор текущего кадра
+
+        // все кадры должны быть одной ширины, равной ширине окна просмотра;
+        // если кадров три, то ширина каждого кадра будет 100/3 = 33.33333%
+        // от ширины контейнера .carousel-slides, то есть 900 пикселей
+        this.allFrames.forEach(frame => frame.style.width = 100/this.length + '%');
+        // ширина цепочки кадров должна равна ширине всех кадров, то есть
+        // 900*3 = 2700 пикселей; но удобнее задать в процентах от родителя,
+        // если кадров три, то ширина контейнера кадров будет 100*3 = 300%
+        this.frameChain.style.width = 100 * this.length + '%';
+
+        this.nextButton.addEventListener('click', event => { // клик по кнопке «вперед»
+            event.preventDefault();
+            this.next();
+        });
+
+        this.prevButton.addEventListener('click', event => { // клик по кнопке «назад»
+            event.preventDefault();
+            this.prev();
+        });
+
+        // клики по кнопкам индикатора текущего кадра
+        this.dotButtons.forEach(dot => {
+            dot.addEventListener('click', event => {
+                event.preventDefault();
+                const index = this.dotButtons.indexOf(event.target);
+                if (index === this.index) return;
+                this.goto(index);
+            });
+        });
+
+        if (this.autoplay) { // включить автоматическую прокрутку?
+            this.play();
+            // когда мышь над слайдером — останавливаем автоматическую прокрутку
+            this.slider.addEventListener('mouseenter', () => clearInterval(this.paused));
+            // когда мышь покидает пределы слайдера — опять запускаем прокрутку
+            this.slider.addEventListener('mouseleave', () => this.play());
+        }
+    }
+
+    // перейти к кадру с индексом index
+    goto(index) {
+        // изменить текущий индекс...
+        if (index > this.length - 1) {
+            this.index = 0;
+        } else if (index < 0) {
+            this.index = this.length - 1;
+        } else {
+            this.index = index;
+        }
+        // ...и выполнить смещение
+        this.move();
+    }
+
+    // перейти к следующему кадру
+    next() {
+        this.goto(this.index + 1);
+    }
+
+    // перейти к предыдущему кадру
+    prev() {
+        this.goto(this.index - 1);
+    }
+
+    // рассчитать и выполнить смещение
+    move() {
+        // на сколько нужно сместить, чтобы нужный кадр попал в окно
+        const offset = 100/this.length * this.index;
+        this.frameChain.style.transform = `translateX(-${offset}%)`;
+        this.dotButtons.forEach(dot => dot.classList.remove('active'));
+        this.dotButtons[this.index].classList.add('active');
+    }
+
+    // запустить автоматическую прокрутку
+    play() {
+        this.paused = setInterval(() => this.next(), 3000);
+    }
+
+    // создать индикатор текущего слайда
+    dots() {
+        const ol = document.createElement('jpg');
+        ol.classList.add('carousel-indicators');
+        const children = [];
+        for (let i = 0; i < this.length; i++) {
+            let li = document.createElement('li');
+            if (i === 0) li.classList.add('active', `in${i}`);
+            /*if (i === 1) li.classList.add('active', `in${i}`);
+            if (i === 2) li.classList.add('active', `in${i}`);
+            if (i === 3) li.classList.add('active', `in${i}`);
+            if (i === 4) li.classList.add('active', `in${i}`);
+            if (i === 5) li.classList.add('active', `in${i}`);
+            if (i === 6) li.classList.add('active', `in${i}`);
+            if (i === 7) li.classList.add('active', `in${i}`);
+            if (i === 8) li.classList.add('active', `in${i}`);
+            if (i === 9) li.classList.add('active', `in${i}`);*/
+            ol.append(li);
+            children.push(li);
+        }
+        this.slider.prepend(ol);
+        return children;
+    }
+}
