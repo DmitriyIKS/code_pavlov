41a23774e24cbabf744b4003be0c56aa1c48d71f
Dmitriy Grey <ssseryyy.ru@gmail.com>
2023-02-21 01:57:09 +0300

    new file:   #Other

---
+++ #Other/разное/lol.js
@@ -0,0 +1,8 @@
+// make massive with 10 random numbers
+var randomNumbers = [];
+for (var i = 0; i < 10; i++) {
+  randomNumbers.push(Math.floor(Math.random() * 100));
+}
+print(randomNumbers);
+
+// create a function that will return a random number
